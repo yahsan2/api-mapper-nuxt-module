b0f2551c877e766cccd3660e38d3bfd36e48db62
Ryosuke Miyamoto <yahsan2@gmail.com>
2019-05-08 21:46:23 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#nuxt-module-api-mapper
+nuxt module for mapping uri and response props of rest api
---
+++ index.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const chalk = require('chalk')
+const debug = require('debug')('nuxt:api')
+
+export default function Api(_moduleOptions) {
+  // Combine options
+  const moduleOptions = _moduleOptions.config
+    ? Object.assign(
+        {},
+        require(_moduleOptions.config.replace(/^(@|~)/, process.cwd())),
+        _moduleOptions
+      )
+    : _moduleOptions
+  const options = Object.assign({}, this.options.api, moduleOptions)
+
+  /* istanbul ignore if */
+  if (process.env.API_URL_BROWSER) {
+    options.browserBaseURL = process.env.API_URL_BROWSER
+  }
+
+  // Default browserBaseURL
+  if (!options.browserBaseURL) {
+    options.browserBaseURL = options.proxy ? prefix : options.baseURL
+  }
+
+  // Register plugin
+  this.addPlugin({
+    src: path.resolve(__dirname, 'plugins/api.template.js'),
+    fileName: 'api.js',
+    options
+  })
+
+  /* eslint-disable no-console */
+  debug(
+    `BaseURL: ${chalk.green(options.baseURL)} (Browser: ${chalk.green(
+      options.browserBaseURL
+    )})`
+  )
+}
